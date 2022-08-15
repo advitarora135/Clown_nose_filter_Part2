7f2d122768ca79d57b9848b632202664558a7d3c
Advit <77945990+advitarora135@users.noreply.github.com>
2022-08-14 17:52:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ function setup() {
     video=createCapture(VIDEO);
     video.size(300,300);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video,0,0,300,300);
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
 }
 function modelLoaded(){
     console.log('Posenet is initialized');
