d74debe898055e74e2845be9ca95d935278aaad2
Advit <77945990+advitarora135@users.noreply.github.com>
2022-08-14 17:44:21 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,300,300);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('Posenet is initialized');
+}
+function take_snapshot(){
+    save(filter.png);
+}
+function gotPoses (results) {
+    if( results > 0){
+        console.log(results);
+        console.log("Nose x = "+ results[0].pose.nose.x);
+        console.log("Nose y = "+ results[0].pose.nose.y);
+    }
+}
